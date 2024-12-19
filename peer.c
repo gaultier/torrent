261be41a6be664479d643683d09785c0c174e715
Philippe Gaultier <philippe.gaultier@gi-de.com>
2024-12-19 14:10:42 +0100

    add peer msg structs

--- peer.c
+++ peer.c
@@ -2,6 +2,32 @@
 
 #include "tracker.c"
 
+typedef struct {
+  u32 index, begin, length;
+} PeerMessageRequest;
+
+typedef struct {
+  u32 index, begin;
+  String data;
+} PeerMessagePiece;
+
+typedef struct {
+  u32 index, begin, length;
+} PeerMessageCancel;
+
+typedef enum {
+  PEER_MSG_KIND_NONE = -1,
+  PEER_MSG_KIND_CHOKE = 0,
+  PEER_MSG_KIND_UNCHOKE = 1,
+  PEER_MSG_KIND_INTERESTED = 2,
+  PEER_MSG_KIND_UNINTERESTED = 3,
+  PEER_MSG_KIND_HAVE = 4,
+  PEER_MSG_KIND_BITFIELD = 5,
+  PEER_MSG_KIND_REQUEST = 6,
+  PEER_MSG_KIND_PIECE = 7,
+  PEER_MSG_KIND_CANCEL = 8,
+} PeerMessageKind;
+
 [[noreturn]]
 static void peer_run(Peer peer, Arena *arena) {
   log(LOG_LEVEL_INFO, "running peer", arena, L("ipv4", peer.ipv4),

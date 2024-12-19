808c01480daadfad61431bc68fa4ba7df2e31997
Philippe Gaultier <philippe.gaultier@gi-de.com>
2024-12-19 11:30:44 +0100

    read torrent file dynamically

--- main.c
+++ main.c
@@ -1,19 +1,21 @@
 #include "tracker.c"
 
-int main() {
+int main(int argc, char *argv[]) {
+  ASSERT(argc == 2);
+
   Arena arena = arena_make_from_virtual_mem(4 * KiB);
-  // TODO: read from file.
-  String torrent_file_content = S(
-      "d8:announce43:http://OpenBSD.somedomain.net:6969/"
-      "announce7:comment107:OpenBSD/7.4/alpha/install74.iso\nCreated by andrew "
-      "fresh (andrew@afresh1.com)\n"
-      "http://OpenBSD.somedomain.net/10:created by13:mktorrent 1.113:creation "
-      "datei1697360758e4:infod6:lengthi234883072e4:name31:OpenBSD_7.4_alpha_"
-      "install74.iso12:piece "
-      "lengthi262144e6:pieces8:abcdefghe8:url-list65:http://"
-      "openbsd.somedomain.net/pub/OpenBSD_7.4_alpha_install74.isoe");
+
+  String torrent_file_path = cstr_to_string(argv[1]);
+  ReadFileResult res_torrent_file_read =
+      file_read_full(torrent_file_path, &arena);
+  if (0 != res_torrent_file_read.error) {
+    log(LOG_LEVEL_ERROR, "read torrent file", &arena,
+        L("err", res_torrent_file_read.error), L("path", torrent_file_path));
+    return errno;
+  }
+
   DecodeMetaInfoResult res_decode_metainfo =
-      decode_metainfo(torrent_file_content, &arena);
+      decode_metainfo(res_torrent_file_read.content, &arena);
   if (STATUS_OK != res_decode_metainfo.status) {
     log(LOG_LEVEL_ERROR, "decode metainfo", &arena,
         L("err", res_decode_metainfo.status));

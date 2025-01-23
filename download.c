6cbc03a592fc2869b36a796cd818d5678ed41a76
Philippe Gaultier <philippe.gaultier@gi-de.com>
2025-01-23 09:39:24 +0100

    fix computing block length

--- download.c
+++ download.c
@@ -58,6 +58,13 @@ download_compute_blocks_count_for_piece(u32 piece, u64 piece_length,
 download_compute_block_length(u32 block, u64 piece_length) {
   PG_ASSERT(block * BLOCK_SIZE < piece_length);
   u32 res = (u32)(piece_length - (u64)block * BLOCK_SIZE) % BLOCK_SIZE;
+  if (0 == res) {
+    res = BLOCK_SIZE;
+  }
+
+  PG_ASSERT(res > 0);
+  PG_ASSERT(res <= BLOCK_SIZE);
+
   return res;
 }
 

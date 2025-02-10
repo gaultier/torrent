27a8fcbc675fdc4c45d0df2f6ff6c488dfbaea64
Philippe Gaultier <philippe.gaultier@gi-de.com>
2025-02-10 15:08:56 +0100

    log

--- main.c
+++ main.c
@@ -18,7 +18,7 @@ static void download_on_timer(uv_timer_t *timer) {
 int main(int argc, char *argv[]) {
   PG_ASSERT(argc == 2);
 
-  PgLogger logger = pg_log_make_logger_stdout_logfmt(PG_LOG_LEVEL_DEBUG);
+  PgLogger logger = pg_log_make_logger_stdout_logfmt(PG_LOG_LEVEL_INFO);
   PgRng rng = pg_rand_make();
 
   PgArena arena = pg_arena_make_from_virtual_mem(1 * PG_MiB);

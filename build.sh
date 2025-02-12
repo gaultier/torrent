19917b3a24257278d88699b07e1bff0af829db00
Philippe Gaultier <philippe.gaultier@gi-de.com>
2025-02-12 10:36:06 +0100

    always use -march=native

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ set -e
 set -f # disable globbing.
 
 CFLAGS="${CFLAGS}"
-EXTRA_FLAGS="-fpie -L./submodules/libuv/build/ -luv -flto -fno-omit-frame-pointer -gsplit-dwarf"
+EXTRA_FLAGS="-fpie -L./submodules/libuv/build/ -luv -flto -fno-omit-frame-pointer -gsplit-dwarf -march=native"
 CC="${CC:-clang}"
 WARNINGS="$(tr -s '\n' ' ' < compile_flags.txt)"
 
@@ -18,13 +18,13 @@ case $1 in
     EXTRA_FLAGS="${EXTRA_FLAGS} -O0"
     ;;
   sanitizer)
-    EXTRA_FLAGS="${EXTRA_FLAGS} -fsanitize=address,undefined -fsanitize-trap=all"
+    EXTRA_FLAGS="${EXTRA_FLAGS} -O0 -fsanitize=address,undefined -fsanitize-trap=all"
     ;;
   release)
-    EXTRA_FLAGS="${EXTRA_FLAGS} -O3 -march=native -flto"
+    EXTRA_FLAGS="${EXTRA_FLAGS} -O3"
     ;;
   release_sanitizer)
-    EXTRA_FLAGS="${EXTRA_FLAGS} -O1 -march=native -fsanitize=address,undefined -fsanitize-trap=all"
+    EXTRA_FLAGS="${EXTRA_FLAGS} -O1 -fsanitize=address,undefined -fsanitize-trap=all"
     ;;
 	*)
 		error "Build mode \"$1\" unsupported!"

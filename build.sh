0b1a84b6dbe0d6e7e530d4abc5c0247c211e9bde
Philippe Gaultier <philippe.gaultier@gi-de.com>
2025-02-12 12:08:27 +0100

    build flags

--- build.sh
+++ build.sh
@@ -21,10 +21,10 @@ case $1 in
     EXTRA_FLAGS="${EXTRA_FLAGS} -O0 -fsanitize=address,undefined -fsanitize-trap=all"
     ;;
   release)
-    EXTRA_FLAGS="${EXTRA_FLAGS} -O3"
+    EXTRA_FLAGS="${EXTRA_FLAGS} -O3 -Wl,--gc-sections"
     ;;
   release_sanitizer)
-    EXTRA_FLAGS="${EXTRA_FLAGS} -O1 -fsanitize=address,undefined -fsanitize-trap=all"
+    EXTRA_FLAGS="${EXTRA_FLAGS} -O1 -fsanitize=address,undefined -fsanitize-trap=all -Wl,--gc-sections"
     ;;
 	*)
 		error "Build mode \"$1\" unsupported!"
@@ -32,7 +32,7 @@ case $1 in
 esac
 
 # shellcheck disable=SC2086
-$CC $WARNINGS -g3 main.c -o main.bin $EXTRA_FLAGS $CFLAGS -Wl,--gc-sections
+$CC $WARNINGS -g3 main.c -o main.bin $EXTRA_FLAGS $CFLAGS
 }
 
 if [ $# -eq 0 ]; then

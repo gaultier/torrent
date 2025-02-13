9e71b43c31b9b1890948245ab677260ee8ccb8ad
Philippe Gaultier <philippe.gaultier@gi-de.com>
2025-02-13 16:51:56 +0100

    test

--- test.sh
+++ test.sh
@@ -2,8 +2,10 @@
 set -e
 set -f # disable globbing.
 
+CFLAGS="${CFLAGS} -fpie -flto -fno-omit-frame-pointer -gsplit-dwarf -march=native -fuse-ld=lld -I./submodules/libuv/include -I./submodules/aws-lc/include"
+LDFLAGS="${LDFLAGS} -L./submodules/libuv/build/ -L./submodules/aws-lc/build/ -luv -lcrypto -Wl,--gc-sections"
 CC="${CC:-clang}"
 WARNINGS="$(tr -s '\n' ' ' < compile_flags.txt)"
 
 # shellcheck disable=SC2086
-"$CC" -O0 $WARNINGS -g3 test.c -o test.bin -fsanitize=address,undefined -fsanitize-trap=all -fpie && ASAN_OPTIONS='detect_leaks=0' ./test.bin
+$CC -O0 $WARNINGS -g3 test.c -o test.bin -fsanitize=address,undefined -fsanitize-trap=all $CFLAGS $LDFLAGS && ASAN_OPTIONS='detect_leaks=0' ./test.bin

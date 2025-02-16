e5a0def06a3637e74d2d072d6cb3f206924751e1
Philippe Gaultier <philippe.gaultier@gi-de.com>
2025-02-17 00:07:52 +0100

    buildflags

--- build-zig.sh
+++ build-zig.sh
@@ -7,4 +7,4 @@ BUILD_DIR="build-$TARGET"
 
 cd submodules/libuv && cmake -G Ninja -S . -B "$BUILD_DIR" -DCMAKE_INSTALL_MESSAGE=NEVER -DCMAKE_MESSAGE_LOG_LEVEL="ERROR" -DCMAKE_BUILD_TYPE=Release -DCMAKE_POSITION_INDEPENDENT_CODE=ON -DLIBUV_BUILD_SHARED=OFF -DCMAKE_C_COMPILER=zig-cc -DCMAKE_C_FLAGS="--target=$TARGET -static -ffunction-sections -fdata-sections" && ninja -C "$BUILD_DIR" libuv.a && cd ../../
 
-CC='zig cc' CFLAGS="--target=$TARGET -ffunction-sections -fdata-sections -Wno-unused-command-line-argument" LDFLAGS="-static -L./submodules/libuv/$BUILD_DIR" ./build.sh release
+CC='zig cc' CFLAGS="$CFLAGS --target=$TARGET -ffunction-sections -fdata-sections -Wno-unused-command-line-argument" LDFLAGS="$LDFLAGS -static -L./submodules/libuv/$BUILD_DIR" sh -x ./build.sh release

125872b54150282cce99750be22877d7958567d7
Philippe Gaultier <philippe.gaultier@gi-de.com>
2025-02-21 12:02:02 +0100

    [wip]

--- build.zig
+++ build.zig
@@ -25,6 +25,15 @@ const cflags: []const []const u8 = &.{
     //"-v",
 };
 
+const targets: []const std.Target.Query = &.{
+    .{ .cpu_arch = .x86_64, .os_tag = .macos },
+    .{ .cpu_arch = .aarch64, .os_tag = .macos },
+    .{ .cpu_arch = .aarch64, .os_tag = .linux },
+    .{ .cpu_arch = .x86_64, .os_tag = .linux, .abi = .gnu },
+    .{ .cpu_arch = .x86_64, .os_tag = .linux, .abi = .musl },
+    .{ .cpu_arch = .x86_64, .os_tag = .windows },
+};
+
 // Although this function looks imperative, note that its job is to
 // declaratively construct a build graph that will be executed by an external
 // runner.
@@ -40,51 +49,53 @@ pub fn build(b: *std.Build) void {
     // set a preferred release mode, allowing the user to decide how to optimize.
     const optimize = b.standardOptimizeOption(.{});
 
-    const exe = b.addExecutable(.{
-        .name = "torrent",
-        .target = target,
-        .optimize = optimize,
-    });
+    for (targets) |t| {
+        const exe = b.addExecutable(.{
+            .name = "torrent",
+            .target = b.resolveTargetQuery(t),
+            .optimize = optimize,
+        });
 
-    exe.addCSourceFiles(.{
-        .files = c_sources,
-        .flags = cflags,
-    });
+        exe.addCSourceFiles(.{
+            .files = c_sources,
+            .flags = cflags,
+        });
 
-    const libuv_dep = b.dependency("libuv", .{
-        .target = target,
-        .optimize = optimize,
-    });
-    exe.linkLibrary(libuv_dep.artifact("uv"));
-    exe.linkLibC();
+        const libuv_dep = b.dependency("libuv", .{
+            .target = target,
+            .optimize = optimize,
+        });
+        exe.linkLibrary(libuv_dep.artifact("uv"));
+        exe.linkLibC();
 
-    //exe.addIncludePath(libuv_dep.path("include"));
+        //exe.addIncludePath(libuv_dep.path("include"));
 
-    // This declares intent for the executable to be installed into the
-    // standard location when the user invokes the "install" step (the default
-    // step when running `zig build`).
-    b.installArtifact(exe);
+        // This declares intent for the executable to be installed into the
+        // standard location when the user invokes the "install" step (the default
+        // step when running `zig build`).
+        b.installArtifact(exe);
 
-    // This *creates* a Run step in the build graph, to be executed when another
-    // step is evaluated that depends on it. The next line below will establish
-    // such a dependency.
-    const run_cmd = b.addRunArtifact(exe);
+        // This *creates* a Run step in the build graph, to be executed when another
+        // step is evaluated that depends on it. The next line below will establish
+        // such a dependency.
+        //     const run_cmd = b.addRunArtifact(exe);
 
-    // By making the run step depend on the install step, it will be run from the
-    // installation directory rather than directly from within the cache directory.
-    // This is not necessary, however, if the application depends on other installed
-    // files, this ensures they will be present and in the expected location.
-    run_cmd.step.dependOn(b.getInstallStep());
+        // By making the run step depend on the install step, it will be run from the
+        // installation directory rather than directly from within the cache directory.
+        // This is not necessary, however, if the application depends on other installed
+        // files, this ensures they will be present and in the expected location.
+        //    run_cmd.step.dependOn(b.getInstallStep());
 
-    // This allows the user to pass arguments to the application in the build
-    // command itself, like this: `zig build run -- arg1 arg2 etc`
-    if (b.args) |args| {
-        run_cmd.addArgs(args);
+        // This allows the user to pass arguments to the application in the build
+        // command itself, like this: `zig build run -- arg1 arg2 etc`
+        // if (b.args) |args| {
+        //     run_cmd.addArgs(args);
+        // }
+
+        // This creates a build step. It will be visible in the `zig build --help` menu,
+        // and can be selected like this: `zig build run`
+        // This will evaluate the `run` step rather than the default, which is "install".
+        // const run_step = b.step("run", "Run the app");
+        // run_step.dependOn(&run_cmd.step);
     }
-
-    // This creates a build step. It will be visible in the `zig build --help` menu,
-    // and can be selected like this: `zig build run`
-    // This will evaluate the `run` step rather than the default, which is "install".
-    const run_step = b.step("run", "Run the app");
-    run_step.dependOn(&run_cmd.step);
 }
